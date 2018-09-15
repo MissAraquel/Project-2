425148453f68ab039c63d534652763c284f0c17c
Eleni Pastra <hell-en@Elenis-MacBook-Pro.local>
2018-09-15 12:19:07 -0700

    db now exists

--- models/index.js
+++ models/index.js
@@ -3,51 +3,35 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
+var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var config = require(path.join(__dirname + "/../config/config.json"))[env];
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
 
+// let sequelize;
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-client.sync().then(function () {
-  // Table created
-  return client.create({
-    email : "jean.dupont@gmail.com",
-    password: "adc12345cxd",
-    first_name: 'Jean',
-    last_name: 'Dupont',
-    location: "New York",
-    preference_time: 1
-  });
-});
 
-
-fs.readdirSync(__dirname)
-  .filter(function(file) {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
   })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
+  .forEach(file => {
+    var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
+  // Genaric association
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
